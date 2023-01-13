d7fc5f5adaa57c9b12bb376b5c8b1a01d56f6a14
Koushik Das <sd2021667@gmail.com>
2023-01-13 21:29:03 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Microtask 3
+Deployed in: https://zkvjpl.deta.dev/
+
+Simple API calling using a public geolocation API using latitude and longitude queries to get the address.
+
+Endpoint: Method - GET
+
+```https://zkvjpl.deta.dev/api/geolocation/myPos?lat={latitude}&lon={longitude}```
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PORT = 5000;
 
 app.use('/api/geolocation', require('./routers/api'))
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server listening on port ${PORT}`);
+// });
 
-// module.exports = app;
+module.exports = app;
